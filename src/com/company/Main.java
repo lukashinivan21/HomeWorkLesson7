a752262182fe8b7be607da85c2d8239d15be7237
Ivan <uvanes21@yandex.ru>
2022-01-20 22:22:39 +0300

    Замечания поправлены

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class Main {
     //solution1
-    public static void yearBissextile(int year) {
+    public static void isLeapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println(year + " год является високосным");
         } else {
@@ -12,28 +15,24 @@ public class Main {
 
     //solution2
     public static void versionRecommend(int numberOS, int year) {
-        int currentYear = 2019;
-        if (year < currentYear && numberOS == 0) {
-            System.out.println("Предлагаю вам установить lite-версию для iOS");
-        } else if (year > currentYear && numberOS == 0) {
-            System.out.println("У вас отличный телефон на базе iOS!");
-        }
-        if (year < currentYear && numberOS == 1) {
-            System.out.println("Предлагаю вам установить lite-версию для Android");
-        } else if (year > currentYear && numberOS == 1) {
-            System.out.println("У вас отличный телефон на базе Android!");
-        }
+        int currentYear = LocalDate.now().getYear();
+        String base = "Установите";
+        base += year < currentYear ? " облегченную версию " : " последнюю версию ";
+        base += "для операционной системы ";
+        base += numberOS == 0 ? "iOs" : "Android";
+        System.out.println(base);
+
+
     }
 
     //solution3
     public static int daysOfDelivery(int distanceInKilometres) {
-        int daysAmount = 0;
-        if (distanceInKilometres <= 20) {
-            daysAmount = 1;
-        } else if (distanceInKilometres > 20 && distanceInKilometres <= 60) {
-            daysAmount = 2;
-        } else if (distanceInKilometres > 60 && distanceInKilometres <= 100) {
-            daysAmount = 3;
+        int daysAmount = 1;
+        if (distanceInKilometres > 20 && distanceInKilometres <= 60) {
+            daysAmount += 1;
+        }
+        if (distanceInKilometres > 60 && distanceInKilometres <= 100) {
+            daysAmount += 2;
         }
         return daysAmount;
     }
@@ -52,25 +51,26 @@ public class Main {
 
     //solution5
     public static void arrayMoved(int[] numbers) {
-        for (int i = numbers.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.println(numbers[i] + ".");
-            } else {
-                System.out.print(numbers[i] + ", ");
-            }
+        System.out.println(Arrays.toString(numbers));
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[numbers.length - 1 - i];
         }
+        System.out.println(Arrays.toString(numbers));
+
+
     }
 
     public static void main(String[] args) {
         //solution1
-        yearBissextile(7864);
+        isLeapYear(7864);
         //solution2
-        versionRecommend(1, 2014);
+        versionRecommend(0, 2022);
         //solution3
         System.out.println("Количество дней доставки равно: " + daysOfDelivery(10));
         //solution4
         validator("adfgghokll");
         //solution5
-        arrayMoved(new int[]{2, 6, 8, 12, 14, 23, 29});
+        arrayMoved(new int[]{2, 6, 8, 12, 14, 19, 21});
+
     }
 }
